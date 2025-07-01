7a39722b768e95467b249a2e7b985c5987acb343
Uli Rizki <ulirizki@gmail.com>
2025-07-01 09:22:58 +0700

    update model

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -9,7 +9,7 @@ class Kategori extends Model
 {
     use HasFactory;
 
-     protected $table = 'kategori';
+    protected $table = 'kategori';
     protected $primaryKey = 'kategori_id';
 
     protected $fillable = [
--- app/Models/Media.php
+++ app/Models/Media.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Media extends Model
 {
     use HasFactory;
+
+    protected $table = 'media';
+    protected $primaryKey = 'media_id';
+
+    protected $fillable = [
+        'mahasiswa_id',
+        'kategori_id',
+        'judul',
+        'deskripsi',
+        'judul_penelitian',
+        'tahun_terbit',
+        'link_media',
+        'gambar_cover'
+    ];
 }
